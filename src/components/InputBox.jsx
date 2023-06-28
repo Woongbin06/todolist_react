52aafae9e33c164f94605203f81ae471d6e72182
Woongbin06 <kob05314@gmail.com>
2023-06-28 22:12:33 +0900

    FEAT :: 할 일 추가

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,18 +1,63 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const InputBox = () => (
-  <div className="todoapp__inputbox">
-    {/* input */}
-    <input
-      type="text"
-      name="todoItem"
-      placeholder="할 일을 입력해주세요"
-      className="todoapp__inputbox-inp"
-    />
-    <button type="submit" className="todoapp__inputbox-add-btn">
-      추가
-    </button>
-  </div>
-);
+const InputBox = ({ todoList, setTodoList }) => {
+  
+  const [text, setText] = useState('');
+  const inputRef = useRef(null); 
+
+  const onChangeInput = (e) => {
+    setText(e.target.value);
+  };
+
+  const onClickAddButton = () => {
+    // todoList에 값 추가
+    const nextTodoList = todoList.concat({
+      id: todoList.length,
+      text,
+    });
+    setTodoList(nextTodoList);
+
+    setText('');
+    inputRef.current.focus();
+  };
+
+  useEffect(() => {  // todoList가 변했을때만 실행
+    console.log(todoList);
+  }, [todoList]);
+
+  return (
+    <div className="todoapp__inputbox">
+      {/* input */}
+      <input
+        type="text"
+        name="todoItem"
+        value={text}
+        ref={inputRef}
+        placeholder="할 일을 입력해주세요"
+        className="todoapp__inputbox-inp"
+        onChange={onChangeInput}
+      />
+      <button 
+        type="submit"
+        className="todoapp__inputbox-add-btn"
+        onClick={onClickAddButton}
+      >
+        추가
+      </button>
+    </div>
+  );
+};
+
+// props 값 검증
+InputBox.propTypes = {
+  todoList: PropTypes.arrayOf( 
+    PropTypes.shape({ 
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    }).isRequired
+  ),
+  setTodoList: PropTypes.func.isRequired,
+};
 
 export default InputBox;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputBox from '../components/InputBox';
 import ToDoList from '../components/ToDoList';
 
-const Home = () => (
-  <div className="homepage__container">
-    {/* ToDo를 추가할 수 있는 input 박스 */}
-    <InputBox />
+const Home = () => {
 
-    {/* 할 일 ToDo 리스트 */}
-    <ToDoList />
+  const [todoList, setTodoList] = useState([]);
 
-    {/* 완료한 ToDp 리스트 */}
-    <ToDoList />
-  </div>
-);
+  return (
+    <div className="homepage__container">
+      {/* ToDo를 추가할 수 있는 input 박스 */}
+      <InputBox todoList={todoList} setTodoList={setTodoList}/>
+
+      {/* 할 일 ToDo 리스트 */}
+      <ToDoList />
+
+      {/* 완료한 ToDp 리스트 */}
+      <ToDoList />
+    </div>
+  );
+};
 
 export default Home;
